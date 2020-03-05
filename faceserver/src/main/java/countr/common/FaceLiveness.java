c83d007cd5491b9f79c7def2a234b81bd7f99104
Darius Lee <dariuszlee@outlook.com>
2020-03-05 17:26:31 +0100

    FaceLiveness Test created and model loading

--- faceserver/src/main/java/countr/common/FaceLiveness.java
+++ faceserver/src/main/java/countr/common/FaceLiveness.java
@@ -1,4 +1,24 @@
+package countr.common;
+
+import java.io.FileInputStream;
+import java.io.File;
+
+import org.deeplearning4j.nn.graph.ComputationGraph;
+import org.deeplearning4j.nn.modelimport.keras.KerasModelImport;
 
 public class FaceLiveness {
-    
+    private final ComputationGraph faceLivenessNetwork;
+    public FaceLiveness(){
+        try {
+            FileInputStream modelStream = new FileInputStream(new File("../Face-Liveness-Detection/models/anandfinal.hdf5"));
+            faceLivenessNetwork = KerasModelImport.importKerasModelAndWeights(modelStream);
+        }
+        catch (Exception e){
+            throw new 
+        }
+    }        
+
+    public void status(){
+        System.out.println("Running...");
+    }
 }
---
+++ faceserver/src/test/java/faceserver/FaceLivenessTest.java
@@ -0,0 +1,10 @@
+package faceserver;
+
+import org.junit.Test;
+
+public class FaceLivenessTest {
+    @Test
+    public void LoadAndBasicTest(){
+        FaceLiveness livenessDetector = new FaceLiveness();
+    }
+}
